db61fe159f9aba815f33b1a1baa73cee392af7c9
farhatrkhalil <khalilrfarhat297@gmail.com>
2024-03-02 18:03:19 +0200

    Implemented counter,clear & vertical view in result page

--- app/src/main/java/com/example/voxelvisage/ResultActivity.java
+++ app/src/main/java/com/example/voxelvisage/ResultActivity.java
@@ -5,32 +5,35 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.bumptech.glide.Glide;
-import com.example.voxelvisage.R;
 
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class ResultActivity extends AppCompatActivity {
 
-    private ImageView imageView;
+    private LinearLayout imageContainer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        imageView = findViewById(R.id.imageView);
+        imageContainer = findViewById(R.id.imageContainer);
 
         Intent intent = getIntent();
         if (intent != null) {
+            ArrayList<String> imageFilePaths = intent.getStringArrayListExtra("imageFilePaths");
             ArrayList<Uri> selectedImages = intent.getParcelableArrayListExtra("selectedImages");
-            if (selectedImages != null && !selectedImages.isEmpty()) {
-                displayImage(selectedImages.get(0));
+
+            if (imageFilePaths != null && !imageFilePaths.isEmpty()) {
+                displayImagesPaths(imageFilePaths);
+            } else if (selectedImages != null && !selectedImages.isEmpty()) {
+                displayImagesUris(selectedImages);
             }
         }
 
@@ -40,21 +43,45 @@ public class ResultActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            navigateToGalleryActivity();
+            navigateBack();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void navigateToGalleryActivity() {
+    private void navigateBack() {
         Intent intent = new Intent(this, GalleryViewerActivity.class);
         startActivity(intent);
         finish();
     }
 
-    private void displayImage(Uri imageUri) {
-        Glide.with(this)
-                .load(imageUri)
-                .into(imageView);
+    private void displayImagesPaths(ArrayList<String> imageFilePaths) {
+        for (String filePath : imageFilePaths) {
+            ImageView imageView = new ImageView(this);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    0, 1f);
+            layoutParams.setMargins(0, 10, 0, 0);
+            imageView.setLayoutParams(layoutParams);
+
+            imageView.setImageURI(Uri.parse("file://" + filePath));
+
+            imageContainer.addView(imageView);
+        }
+    }
+
+    private void displayImagesUris(ArrayList<Uri> selectedImages) {
+        for (Uri imageUri : selectedImages) {
+            ImageView imageView = new ImageView(this);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    0, 1f);
+            layoutParams.setMargins(0, 10, 0, 0);
+            imageView.setLayoutParams(layoutParams);
+
+            imageView.setImageURI(imageUri);
+
+            imageContainer.addView(imageView);
+        }
     }
 }
--- app/src/main/java/com/example/voxelvisage/ui/camera/CameraFragment.java
+++ app/src/main/java/com/example/voxelvisage/ui/camera/CameraFragment.java
@@ -1,59 +1,256 @@
 package com.example.voxelvisage.ui.camera;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 import com.example.voxelvisage.R;
+import com.example.voxelvisage.ResultActivity;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class CameraFragment extends Fragment {
 
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    public static final String IMAGE_FILE_PATHS_KEY = "imageFilePaths";
+    private static final int MAX_IMAGES = 5;
+    private int capturedImages = 0;
     private ImageView cameraView;
+    private TextView counterTextView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_camera, container, false);
 
-        // Initialize views
         cameraView = rootView.findViewById(R.id.CameraView);
         Button captureButton = rootView.findViewById(R.id.Button);
+        counterTextView = rootView.findViewById(R.id.CounterTextView);
 
-        // Set click listener for the "Take Picture" button
+        updateCounterText();
+
         captureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Create an intent to open the camera
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureIntent.resolveActivity(requireActivity().getPackageManager()) != null) {
-                    startActivityForResult(takePictureIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+                if (capturedImages < MAX_IMAGES) {
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (takePictureIntent.resolveActivity(requireActivity().getPackageManager()) != null) {
+                        startActivityForResult(takePictureIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+                    }
+                } else {
+                    showMaxImagesPopup();
                 }
             }
         });
 
+        if (savedInstanceState == null) {
+            showInstructionsPopup();
+        }
+
+        setHasOptionsMenu(true);
+
         return rootView;
     }
 
-    // Handle the result after capturing an image
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            // Get the captured image and display it in the ImageView
             Bundle extras = data.getExtras();
             if (extras != null) {
-                // The key "data" contains the captured image
-                Bitmap capturedImage = (Bitmap) extras.get("data");
-                cameraView.setImageBitmap(capturedImage);
+
+                Bitmap capturedImage = (Bitmap) data.getExtras().get("data");
+                String imagePath = saveImageToFile(capturedImage);
+
+                loadFullResolutionImage(imagePath);
+
+                capturedImages++;
+                updateCounterText();
+
+                showImageSavedToast();
+
+                if (capturedImages == MAX_IMAGES) {
+                    showCompletionPopup();
+                }
             }
         }
+    }
+
+    private String saveImageToFile(Bitmap imageBitmap) {
+        File directory = requireContext().getDir("images", Context.MODE_PRIVATE);
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String fileName = "IMG_" + timeStamp + ".jpg";
+
+        File file = new File(directory, fileName);
+
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(requireContext(), "Error saving image", Toast.LENGTH_SHORT).show();
+        }
+
+        return file.getAbsolutePath();
+    }
+
+    private void loadFullResolutionImage(String imagePath) {
+        if (imagePath != null) {
+            Bitmap fullResolutionImage = BitmapFactory.decodeFile(imagePath);
+            cameraView.setImageBitmap(fullResolutionImage);
+        }
+    }
+
+    private void updateCounterText() {
+        counterTextView.setText(String.format("%d/%d", capturedImages, MAX_IMAGES));
+    }
+
+    private void showInstructionsPopup() {
+        new AlertDialog.Builder(requireContext())
+                .setTitle("Instructions")
+                .setMessage("Capture 5 images, and then click on proceed to navigate to the result page.")
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        triggerTakePictureButtonClick();
+                    }
+                })
+                .show();
+    }
+
+    private void triggerTakePictureButtonClick() {
+        Button captureButton = requireView().findViewById(R.id.Button);
+
+        captureButton.performClick();
+    }
+
+    private void showImageSavedToast() {
+        Toast.makeText(requireContext(), "Image Number " + capturedImages + " Saved", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showCompletionPopup() {
+        new AlertDialog.Builder(requireContext())
+                .setTitle("Capture Completed")
+                .setMessage("Do you want to proceed or reset?")
+                .setPositiveButton("Proceed", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        navigateToResultPage();
+                    }
+                })
+                .setNegativeButton("Reset", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        resetCapture();
+                    }
+                })
+                .show();
+    }
+    private void navigateToResultPage() {
+        Intent resultIntent = new Intent(requireContext(), ResultActivity.class);
+
+        ArrayList<String> imageFilePaths = new ArrayList<>();
+
+        for (int i = 1; i <= MAX_IMAGES; i++) {
+            String fileName = String.format("IMG_%d.jpg", i);
+            File imageFile = new File(requireContext().getDir("images", Context.MODE_PRIVATE), fileName);
+            if (imageFile.exists()) {
+                imageFilePaths.add(imageFile.getAbsolutePath());
+            }
+        }
+
+
+        resultIntent.putStringArrayListExtra("imageFilePaths", imageFilePaths);
+
+        startActivity(resultIntent);
+    }
+
+
+
+    private void showMaxImagesPopup() {
+        new AlertDialog.Builder(requireContext())
+                .setTitle("Maximum Images Captured")
+                .setMessage("You have captured the maximum number of images.")
+                .setPositiveButton(android.R.string.ok, null)
+                .show();
+    }
+
+    private void resetCapture() {
+        capturedImages = 0;
+        updateCounterText();
+        cameraView.setImageBitmap(null);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.capture_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_button_remove) {
+            showClearImagePopup();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showClearImagePopup() {
+        if (capturedImages > 0) {
+            new AlertDialog.Builder(requireContext())
+                    .setTitle("Clear Captured Image")
+                    .setMessage("Are you sure you want to remove the captured image/s?")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            clearCapturedImage();
+                        }
+                    })
+                    .setNegativeButton("Cancel", null)
+                    .show();
+        } else {
+            showNoImagesPopup();
+        }
+    }
+
+    private void showNoImagesPopup() {
+        new AlertDialog.Builder(requireContext())
+                .setTitle("No Images Captured")
+                .setMessage("There are no captured images to remove.")
+                .setPositiveButton(android.R.string.ok, null)
+                .show();
+    }
+
+    private void clearCapturedImage() {
+        capturedImages = 0;
+        updateCounterText();
+        cameraView.setImageBitmap(null);
+        Toast.makeText(requireContext(), "Captured image removed", Toast.LENGTH_SHORT).show();
     }
 }
