13a0cf646beb97efa98e8d6e1891d6a9acab7c7a
farhatrkhalil <khalilrfarhat297@gmail.com>
2024-03-11 18:53:07 +0200

    Implemented dark mode

--- app/src/main/java/com/example/voxelvisage/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/voxelvisage/ui/settings/SettingsFragment.java
@@ -1,83 +1,98 @@
 package com.example.voxelvisage.ui.settings;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.CheckBoxPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
 import com.example.voxelvisage.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
+    @RequiresApi(api = Build.VERSION_CODES.R)
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
 
+        CheckBoxPreference darkModePreference = findPreference("darkMode");
+        assert darkModePreference != null;
+
+        int currentNightMode = AppCompatDelegate.getDefaultNightMode();
+        darkModePreference.setChecked(currentNightMode == AppCompatDelegate.MODE_NIGHT_YES);
+
+        darkModePreference.setOnPreferenceClickListener(preference -> {
+            boolean isDarkModeEnabled = darkModePreference.isChecked();
+            updateTheme(isDarkModeEnabled);
+            return true;
+        });
+
         Preference developersPreference = findPreference("developers_info");
         assert developersPreference != null;
-        developersPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(@NonNull Preference preference) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
-                builder.setTitle("Developers");
-                builder.setMessage("Voxel Visage is developed by:\n\n" +
-                        "Abed El Fattah Amouneh\n" +
-                        "Khalil Farhat\n" +
-                        "Sammy Medawar\n" +
-                        "Wally El Sayed");
+        developersPreference.setOnPreferenceClickListener(preference -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+            builder.setTitle("Developers");
+            builder.setMessage("Voxel Visage is developed by:\n\n" +
+                    "Abed El Fattah Amouneh\n" +
+                    "Khalil Farhat\n" +
+                    "Sammy Medawar\n" +
+                    "Wally El Sayed");
 
-                builder.setPositiveButton("OK", (dialog, which) -> {
-                    dialog.dismiss();
-                });
+            builder.setPositiveButton("OK", (dialog, which) -> dialog.dismiss());
 
-                builder.show();
+            builder.show();
 
-                return true;
-            }
+            return true;
         });
 
         Preference privacyPolicyPreference = findPreference("privacy_policy");
         assert privacyPolicyPreference != null;
-        privacyPolicyPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(@NonNull Preference preference) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
-                builder.setTitle("Privacy Policy");
-                builder.setMessage("Voxel Visage Privacy Policy:\n\n" +
-                        "Your privacy is important to us. This app does not collect personal information.");
+        privacyPolicyPreference.setOnPreferenceClickListener(preference -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+            builder.setTitle("Privacy Policy");
+            builder.setMessage("Voxel Visage Privacy Policy:\n\n" +
+                    "Your privacy is important to us. This app does not collect personal information.");
 
-                builder.setPositiveButton("OK", (dialog, which) -> {
-                    dialog.dismiss();
-                });
+            builder.setPositiveButton("OK", (dialog, which) -> dialog.dismiss());
 
-                builder.show();
+            builder.show();
 
-                return true;
-            }
+            return true;
         });
 
         Preference licensePreference = findPreference("license");
         assert licensePreference != null;
-        licensePreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(@NonNull Preference preference) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
-                builder.setTitle("App License");
-                builder.setMessage("This app is licensed under the MIT License. " +
-                        "The MIT License is a type of software license that allows you, the user, a great deal of freedom. " +
-                        "It means you can use, modify, and distribute this app according to the terms of the MIT License. " +
-                        "You are free to do so as long as you provide appropriate attribution or credit to the original authors.\n" +
-                        "Thank you for using our app!");
+        licensePreference.setOnPreferenceClickListener(preference -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+            builder.setTitle("App License");
+            builder.setMessage("This app is licensed under the MIT License. " +
+                    "The MIT License is a type of software license that allows you, the user, a great deal of freedom. " +
+                    "It means you can use, modify, and distribute this app according to the terms of the MIT License. " +
+                    "You are free to do so as long as you provide appropriate attribution or credit to the original authors.\n" +
+                    "Thank you for using our app!");
 
+            builder.setPositiveButton("OK", (dialog, which) -> dialog.dismiss());
 
-                builder.setPositiveButton("OK", (dialog, which) -> {
-                    dialog.dismiss();
-                });
+            builder.show();
 
-                builder.show();
-
-                return true;
-            }
+            return true;
         });
     }
+
+    private void updateTheme(boolean isDarkModeEnabled) {
+        int nightMode = isDarkModeEnabled ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO;
+        AppCompatDelegate.setDefaultNightMode(nightMode);
+
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(requireContext()).edit();
+        editor.putBoolean("darkMode", isDarkModeEnabled);
+        editor.apply();
+    }
+
 }
