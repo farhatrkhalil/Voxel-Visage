1de7e4abd4f440e67b64e0defb701ff5d884ee2c
farhatrkhalil <khalilrfarhat297@gmail.com>
2024-03-14 20:38:21 +0200

    Implemented lazy loading for images.

--- app/src/main/java/com/example/voxelvisage/GalleryAdapter.java
+++ app/src/main/java/com/example/voxelvisage/GalleryAdapter.java
@@ -19,6 +19,9 @@ public class GalleryAdapter extends BaseAdapter {
     private final Context context;
     private final ArrayList<Uri> images;
     private final boolean[] selectedStates;
+    private int visibleItemCount = 15;
+    private int loadedItemCount = 0;
+
 
     public GalleryAdapter(Context context, ArrayList<Uri> images, boolean[] selectedStates) {
         this.context = context;
@@ -28,7 +31,7 @@ public class GalleryAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return images.size();
+        return Math.min(images.size(), loadedItemCount + visibleItemCount);
     }
 
     @Override
@@ -43,34 +46,52 @@ public class GalleryAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
         if (convertView == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.grid_item, parent, false);
+            holder = new ViewHolder();
+            holder.imageView = convertView.findViewById(R.id.imageView);
+            holder.tickImageView = convertView.findViewById(R.id.tickImageView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
         }
 
-        CurvedImageView imageView = convertView.findViewById(R.id.imageView);
-        ImageView tickImageView = convertView.findViewById(R.id.tickImageView);
-
-        Picasso.get().load(images.get(position)).resize(300, 300).centerCrop().into(imageView);
+        Uri imageUri = images.get(position);
+        Picasso.get().load(imageUri).resize(600, 400).centerCrop().into(holder.imageView);
 
         if (selectedStates[position]) {
-            tickImageView.setVisibility(View.VISIBLE);
+            holder.tickImageView.setVisibility(View.VISIBLE);
         } else {
-            tickImageView.setVisibility(View.GONE);
+            holder.tickImageView.setVisibility(View.GONE);
             convertView.setBackgroundColor(ContextCompat.getColor(context, android.R.color.transparent));
+        }
+
+        if (position >= getCount() - 1 && loadedItemCount < images.size()) {
+            loadedItemCount += visibleItemCount;
+            notifyDataSetChanged();
         }
 
         return convertView;
     }
 
+    static class ViewHolder {
+        CurvedImageView imageView;
+        ImageView tickImageView;
+    }
 
     public void updateView(int position, View convertView) {
-        ImageView tickImageView = convertView.findViewById(R.id.tickImageView);
+        ViewHolder holder = (ViewHolder) convertView.getTag();
 
         if (selectedStates[position]) {
-            tickImageView.setVisibility(View.VISIBLE);
+            holder.tickImageView.setVisibility(View.VISIBLE);
         } else {
-            tickImageView.setVisibility(View.GONE);
+            holder.tickImageView.setVisibility(View.GONE);
             convertView.setBackgroundColor(ContextCompat.getColor(context, android.R.color.transparent));
         }
+    }
+    public void setVisibleItemCount(int itemCount) {
+        visibleItemCount = itemCount;
+        notifyDataSetChanged();
     }
 }
