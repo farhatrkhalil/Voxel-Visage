aa3b6e866d1dfb7c6253a914e83c19281844007a
farhatrkhalil <khalilrfarhat297@gmail.com>
2024-04-08 22:56:15 +0300

    Implemented showEyeGlassSelectionDialog dialog.

--- app/src/main/java/com/voxelvisage/modelviewer/EyeGlassButtonClickListener.kt
+++ app/src/main/java/com/voxelvisage/modelviewer/EyeGlassButtonClickListener.kt
@@ -2,10 +2,21 @@ package com.voxelvisage.modelviewer
 
 import android.content.Context
 import android.view.View
-import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class EyeGlassButtonClickListener(private val context: Context) : View.OnClickListener {
+
     override fun onClick(v: View?) {
-        Toast.makeText(context, "Eye glass button clicked", Toast.LENGTH_SHORT).show()
+        showEyeGlassSelectionDialog()
+    }
+
+    private fun showEyeGlassSelectionDialog() {
+        AlertDialog.Builder(context)
+            .setTitle("Filters Overlaying")
+            .setMessage(
+                "The user can select one of the 3D eyeglasses filters.\n\n" + "Voxel Visage will apply the selected filter to a 3D model face displayed in the scene by fitting the glasses onto the face seamlessly.\n\n" + "If the selected model does not accurately represent a face or is unclear, an error will be displayed.\n"
+            )
+            .setPositiveButton("OK", null)
+            .show()
     }
 }
