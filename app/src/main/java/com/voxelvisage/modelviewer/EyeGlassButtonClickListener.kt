92473678f0469ba2b4a63c478112be6d1d634620
farhatrkhalil <khalilrfarhat297@gmail.com>
2024-04-10 15:54:14 +0300

    Implemented filter dialog options.

--- app/src/main/java/com/voxelvisage/modelviewer/EyeGlassButtonClickListener.kt
+++ app/src/main/java/com/voxelvisage/modelviewer/EyeGlassButtonClickListener.kt
@@ -1,7 +1,5 @@
 package com.voxelvisage.modelviewer
 
-import android.content.Context
-import android.net.Uri
 import android.util.TypedValue
 import android.view.View
 import android.widget.TextView
@@ -31,16 +29,37 @@ class EyeGlassButtonClickListener(private val mainActivity: MainActivity) : View
             return
         }
 
+        val filtermodels: List<String> = mainActivity.assets.list("")?.filter { it.endsWith(".obj") } ?: emptyList()
+
         alertDialogBuilder
             .setTitle("Filters Overlaying")
             .setMessage(
                 "The user can select one of the 3D eyeglasses filters.\n\n" +
                         "Voxel Visage will apply the selected filter to a 3D model face displayed in the scene by fitting the glasses onto the face seamlessly.\n\n" +
-                        "If the selected model does not accurately represent a face or is unclear, an error will be displayed."
+                        "If the selected model does not accurately represent a face or is unclear, an error will be displayed.\n"
             )
-            .setPositiveButton("OK") { _, _ ->
+            .setPositiveButton("OK") { dialog, _ ->
+                // After clicking OK, show another dialog with the options
+                showSelectOptionsDialog()
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
             }
             .show()
     }
 
+    private fun showSelectOptionsDialog() {
+        val options = arrayOf("metal_frame_reading_glasses_filter2.obj", "thin_eyeglasses_filter1.obj")
+
+        val alertDialogBuilder = AlertDialog.Builder(mainActivity)
+        alertDialogBuilder
+            .setTitle("Select a filter:")
+            .setItems(options) { dialog, which ->
+                // Handle option selection here
+                val selectedOption = options[which]
+                // You can perform further actions based on the selected option
+            }
+            .show()
+    }
 }
